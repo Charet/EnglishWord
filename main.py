5c7075c7a3736e9d259d2566a4307f42b4b73366
Charet <2779678916@qq.com>
2022-02-25 16:19:21 +0800

    test

--- main.py
+++ main.py
@@ -2,49 +2,46 @@ import random
 import os
 import time
 import pyttsx3
-import keyboard
 
 true_word = []
 false_word = []
-word = []
+word = {}
 
 
-def readWord(key, words):  # 朗读代码
-    if key.event_type == 'up' and key.name == 'space':
-        engine = pyttsx3.init()
-        engine.say(words)
-        engine.runAndWait()  # 无此句将无法发音
+def readWord(words):  # 朗读代码
+    engine = pyttsx3.init()
+    engine.say(words)
+    print(words)
+    engine.runAndWait()  # 无此句将无法发音
 
 
 def randomWord(length):  # 随机抽取单词
     serial = random.randint(1, length)
-    serial = serial * 2 - 1
-    return serial
+    words = list(word.keys())[serial]
+    return words
 
 
-def compare(serial):
-    print(word[serial])
+def compare(words):
+    print(words)
     input_word = input()
     os.system('cls')
-    if input_word == word[serial - 1]:
+    if input_word == word[words]:
         print("正确!")
-        true_word.append(word[serial] + ' ' + input_word)
-        del word[serial - 1:serial + 1]
+        true_word.append(word[words] + ' ' + input_word)
+        del word[words]
         time.sleep(1)
     else:
-        print("错误!正确单词为:", word[serial - 1], "\n按空格可以朗读此单词\n按Enter可进入下一个单词")
-        false_word.append(word[serial] + ' ' + input_word)
-        del word[serial - 1:serial + 1]
-        keyboard.add_hotkey('space', readWord, args=(word[serial - 1],))
-        keyboard.wait("enter")
+        print("错误!正确单词为:", word[words])
+        false_word.append(words + ' ' + input_word)
+        del word[words]
+        time.sleep(5)
 
 
 def openfile(file):  # 在本地文件中获取要听写的单词表
     for line in open("./words/" + file, "r", encoding='utf-8'):
         lines_english = ' '.join(line.split(' ', )[0:-1])
         lines_chinese = ''.join(line.split(' ', )[-1])
-        word.append(lines_english)
-        word.append(lines_chinese.replace("\n", ""))
+        word[lines_chinese.replace("\n", "")] = lines_english
 
 
 def unit():
@@ -54,7 +51,7 @@ def unit():
 
 def main():
     openfile(unit())
-    length = len(word) / 2  # 计算单词总数
+    length = len(word)  # 计算单词总数
     while length != 0:
         compare(randomWord(length))
         length -= 1
